0d0367a1962452a0deeb70c568fcaf6530f68ee9
ORM-Laptop <knightdeman@gmail.com>
2024-10-10 16:24:34 +0300

    new controller file

--- client/main.js
+++ client/main.js
@@ -20,14 +20,14 @@ app.whenReady().then(createWindow);
 
 ipcMain.on('send-data', async (event, data) => {
   const sock = new zmq.Request();
-  await sock.connect("tcp://localhost:5555");
+  await sock.connect("tcp://192.168.110.202:5555");
 
   console.log("Sending:", data);
   await sock.send(JSON.stringify(data));
   const [result] = await sock.receive();
 
   const camera_socket = new zmq.Request();
-  await camera_socket.connect("tcp://localhost:5556");
+  await camera_socket.connect("tcp://192.168.110.202:5556");
 
   const camera_img = await camera_socket.receive();
   console.log("Received img:", camera_img.toString());
